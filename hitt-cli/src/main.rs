3be27db92fa703e92810c8271a3125a7d556a5b9
Mads Hougesen <mads@mhouge.dk>
2023-10-29 01:26:55 +0200

    feat(cli): implement json pretty printing

--- hitt-cli/src/main.rs
+++ hitt-cli/src/main.rs
@@ -44,17 +44,25 @@ fn print_headers(headers: &reqwest::header::HeaderMap) {
     }
 }
 
-fn print_json(body: &str) {
-    println!("{body}");
+fn format_json(input: &str) -> String {
+    jsonformat::format(input, jsonformat::Indentation::TwoSpace)
+}
+
+fn print_pretty_json(input: &str) {
+    let formatted_json = format_json(input);
+
+    println!("{formatted_json}");
 }
 
 fn print_body(body: &str, content_type: Option<&str>) {
     match content_type {
-        Some(content_type) => match content_type {
-            "application/json" => print_json(body),
-
-            _ => println!("{body}"),
-        },
+        Some(content_type) => {
+            if content_type.starts_with("application/json") {
+                print_pretty_json(body);
+            } else {
+                println!("{body}");
+            }
+        }
         None => println!("{body}"),
     }
 }
