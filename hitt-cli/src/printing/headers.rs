d76aa00d680def0cd4254585c163f831ef3ae672
Mads Hougesen <mads@mhouge.dk>
2023-10-29 22:04:26 +0100

    feat(cli): allow running all directory entries

--- hitt-cli/src/printing/headers.rs
+++ hitt-cli/src/printing/headers.rs
@@ -1,12 +1,14 @@
 use crate::printing::{TEXT_RESET, TEXT_YELLOW};
 
+use super::print_error;
+
 #[inline]
 pub(crate) fn print_headers(headers: &reqwest::header::HeaderMap) {
     for (key, value) in headers {
         if let Ok(value) = value.to_str() {
             println!("{TEXT_YELLOW}{key}{TEXT_RESET}: {value}{TEXT_RESET}");
         } else {
-            eprintln!("Error printing value for header: {key}");
+            print_error(format!("error printing value for header: {key}"));
         }
     }
 }
