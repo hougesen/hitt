25fbad751fc01904d714a031fd1ba01e41dde98d
Mads Hougesen <mads@mhouge.dk>
2023-10-29 15:33:49 +0100

    style(cli): add color coding to response (#20)

--- hitt-cli/src/printing/headers.rs
+++ hitt-cli/src/printing/headers.rs
@@ -1,8 +1,10 @@
+use crate::printing::{TEXT_RESET, TEXT_YELLOW};
+
 #[inline]
 pub(crate) fn print_headers(headers: &reqwest::header::HeaderMap) {
     for (key, value) in headers {
         if let Ok(value) = value.to_str() {
-            println!("{key}: {value}",);
+            println!("{TEXT_YELLOW}{key}{TEXT_RESET}: {value}{TEXT_RESET}");
         } else {
             eprintln!("Error printing value for header: {key}");
         }
--- hitt-cli/src/printing/status.rs
+++ hitt-cli/src/printing/status.rs
@@ -1,4 +1,12 @@
+use crate::printing::{STYLE_BOLD, STYLE_RESET, TEXT_GREEN, TEXT_RED, TEXT_RESET};
+
 #[inline]
 pub(crate) fn print_status(method: &str, url: &str, status_code: u16) {
-    println!("{method} {url} {status_code}");
+    let text_color = if status_code < 400 {
+        TEXT_GREEN
+    } else {
+        TEXT_RED
+    };
+
+    println!("{STYLE_BOLD}{text_color}{method} {url} {status_code}{TEXT_RESET}{STYLE_RESET}");
 }
