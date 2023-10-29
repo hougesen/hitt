d63059b218c10520d45e11d72587f15944f678eb
Mads Hougesen <mads@mhouge.dk>
2023-10-29 02:46:54 +0200

    feat(cli): parse input arguments (#14)

---
+++ hitt-cli/src/config/mod.rs
@@ -0,0 +1,24 @@
+/// Simple program to greet a person
+#[derive(clap::Parser, Debug)]
+#[clap(
+    author = "Mads Hougesen, mhouge.dk",
+    version,
+    about = "hitt is a HTTP testing tool focused on speed and simplicity."
+)]
+pub(crate) struct CliArguments {
+    /// Path to .http file
+    #[arg()]
+    pub(crate) path: String,
+
+    /// Exit on error status code
+    #[arg(long, default_value_t = false)]
+    pub(crate) fail_fast: bool,
+
+    /// Whether or not to show response body
+    #[arg(long, default_value_t = false)]
+    pub(crate) hide_body: bool,
+
+    /// Whether or not to show response headers
+    #[arg(long, default_value_t = false)]
+    pub(crate) hide_headers: bool,
+}
