1960d2a7ad3ad81596e990bfdd1833cad9b5a98e
Mads Hougesen <mads@mhouge.dk>
2023-11-05 21:01:54 +0100

    feat(cli): ignore gitignored files (#35)

--- hitt-cli/src/fs/mod.rs
+++ hitt-cli/src/fs/mod.rs
@@ -1,4 +1,3 @@
-use async_recursion::async_recursion;
 use hitt_request::send_request;
 
 use crate::{
@@ -7,13 +6,9 @@ use crate::{
 };
 
 async fn get_file_content(path: std::path::PathBuf) -> Result<String, std::io::Error> {
-    let buffr = tokio::fs::read(path).await?;
-
-    Ok(String::from_utf8_lossy(&buffr).to_string())
-}
-
-pub(crate) async fn is_directory(path: &std::path::Path) -> Result<bool, std::io::Error> {
-    tokio::fs::metadata(path).await.map(|m| m.is_dir())
+    tokio::fs::read(path)
+        .await
+        .map(|buf| String::from_utf8_lossy(&buf).to_string())
 }
 
 pub(crate) async fn handle_file(
@@ -41,48 +36,23 @@ pub(crate) async fn handle_file(
     Ok(())
 }
 
-async fn handle_dir_entry(
-    http_client: &reqwest::Client,
-    entry: tokio::fs::DirEntry,
-    args: &RunCommandArguments,
-) -> Result<(), std::io::Error> {
-    let metadata = entry.metadata().await?;
+pub(crate) fn find_http_files(path: &std::path::Path) -> Vec<std::path::PathBuf> {
+    ignore::WalkBuilder::new(path)
+        .build()
+        .filter_map(|orginal_entry| {
+            if let Ok(entry) = orginal_entry {
+                let path = entry.path();
 
-    match metadata.is_dir() {
-        true => handle_dir(http_client, entry.path(), args).await,
-        false => {
-            let entry_path = entry.path();
-
-            if let Some(ext) = entry_path.extension() {
-                if ext == "http" || ext == "rest" {
-                    return handle_file(http_client, entry_path, args).await;
+                if let Some(ext) = path.extension() {
+                    if ext == "http" {
+                        return Some(path.to_path_buf());
+                    }
                 }
+
+                None
+            } else {
+                None
             }
-
-            Ok(())
-        }
-    }
-}
-
-#[async_recursion]
-pub(crate) async fn handle_dir(
-    http_client: &reqwest::Client,
-    path: std::path::PathBuf,
-    args: &RunCommandArguments,
-) -> Result<(), std::io::Error> {
-    if !args.recursive {
-        print_error(format!(
-            "{path:?} is a directory, but the recursive argument was not supplied"
-        ));
-
-        std::process::exit(1);
-    }
-
-    let mut read_dir_result = tokio::fs::read_dir(&path).await?;
-
-    while let Some(entry) = read_dir_result.next_entry().await? {
-        handle_dir_entry(http_client, entry, args).await?;
-    }
-
-    Ok(())
+        })
+        .collect()
 }
