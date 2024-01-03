28616b2bce95fbc4d3ecd883024fe1944a6d9b2d
Mads Hougesen <mads@mhouge.dk>
2024-01-03 15:24:37 +0100

    docs: redesign docs

---
+++ docs/components/SideNavigation.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<template>
+  <nav>
+    <ContentNavigation v-slot="{ navigation }">
+      <ul>
+        <li
+          v-for="link of navigation"
+          :key="link._path"
+          class="text-lg font-bold"
+        >
+          <NuxtLink
+            :to="link._path"
+            :class="{
+              'text-blue-400': route?.path === link?._path,
+            }"
+          >
+            {{ link.title }}
+          </NuxtLink>
+        </li>
+
+        <li class="mt-4 text-lg font-bold">
+          <NuxtLink to="https://github.com/hougesen/hitt" target="_blank">
+            GitHub
+          </NuxtLink>
+        </li>
+      </ul>
+    </ContentNavigation>
+  </nav>
+</template>
--- docs/layouts/default.vue
+++ docs/layouts/default.vue
@@ -1,5 +1,27 @@
+<script setup>
+const { page, next, prev } = useContent();
+
+useContentHead(page);
+</script>
+
 <template>
-  <article class="mx-auto container prose lg:prose-xl">
-    <slot></slot>
+  <article class="prose lg:prose-xl">
+    <h1>
+      {{ page.title }}
+    </h1>
+
+    <TableOfContents />
+
+    <ContentRenderer :key="page._id" :value="page" />
+
+    <div class="flex w-full gap-4">
+      <NuxtLink v-if="prev?._path" :to="prev._path"
+        >&lt; {{ prev.title }}</NuxtLink
+      >
+
+      <NuxtLink v-if="next?._path" :to="next._path" class="ml-auto"
+        >{{ next.title }} &gt;</NuxtLink
+      >
+    </div>
   </article>
 </template>
