4713037ea9ae1e780898c68dab3dc7956fa38198
Mads Hougesen <mads@mhouge.dk>
2024-06-10 23:54:09 +0200

    style(docs): flex-col on content

--- docs/layouts/default.vue
+++ docs/layouts/default.vue
@@ -5,14 +5,19 @@ useContentHead(page);
 </script>
 
 <template>
-  <article class="prose lg:prose-xl">
+  <article class="prose lg:prose-xl flex-col">
     <h1>
       {{ page.title }}
     </h1>
 
     <TableOfContents />
 
-    <ContentRenderer :key="page._id" :value="page" class="flex-col" />
+    <ContentRenderer
+      :key="page._id"
+      :value="page"
+      class="flex-col"
+      style="flex-direction: column !important "
+    />
 
     <div class="flex w-full gap-4">
       <NuxtLink v-if="prev?._path" :to="prev._path"
