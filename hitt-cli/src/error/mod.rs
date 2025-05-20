2cfe4d63549c848d637301a19e21c8669445baf8
Mads Hougesen <mads@mhouge.dk>
2025-05-20 13:19:05 +0200

    refactor(cli): heap allocate hitt_sse::Error to reduce enum size

--- hitt-cli/src/error/mod.rs
+++ hitt-cli/src/error/mod.rs
@@ -12,7 +12,7 @@ pub enum HittCliError {
     RecursiveNotEnabled,
     FailFast,
     SSEParseUrl(String),
-    SSEError(hitt_sse::Error),
+    SSEError(Box<hitt_sse::Error>),
 }
 
 impl core::error::Error for HittCliError {}
@@ -27,12 +27,10 @@ impl core::fmt::Display for HittCliError {
             Self::Io(error) => write!(f, "io error {error:#?}"),
             Self::Reqwest(method, uri, error) => write!(f, "{method} {uri} - {error}"),
             Self::RequestTimeout(method, uri) => write!(f, "{method} {uri} - request timed out"),
-            Self::InvalidVariableArgument(input) => {
-                write!(
-                    f,
-                    "'{input}' is not a valid variable argument - variable input should be '--var <KEY>=<VALUE>'"
-                )
-            }
+            Self::InvalidVariableArgument(input) => write!(
+                f,
+                "'{input}' is not a valid variable argument - variable input should be '--var <KEY>=<VALUE>'"
+            ),
             Self::VariableArgumentKeyIndexing(variable) => {
                 write!(f, "unable to index key of --var '{variable}'")
             }
